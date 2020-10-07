6e576fcf10bf712464921f6da3abc92f611df1d0
Guillaume Perbet <perbet.dev@gmail.com>
2020-10-07 14:13:38 +0200

    add deck and decks views

---
+++ resources/views/decks.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Magic - Decks</title>
+</head>
+<body>
+    @foreach ($decks as $deck)
+        <h2>{{ $deck->id }}</h2>
+    @endforeach
+</body>
+</html>
