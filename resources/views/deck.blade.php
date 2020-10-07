2ef578b649a5f62d11e7f741df0801f6ed81b09b
Guillaume Perbet <perbet.dev@gmail.com>
2020-10-07 15:10:30 +0200

    add base template

--- resources/views/deck.blade.php
+++ resources/views/deck.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Magic - Decks</title>
-</head>
-<body>
+@extends('base')
+
+@section('content')
     <h2>{{ $deck->name }}</h2>
-</body>
-</html>
+@endsection
--- resources/views/decks.blade.php
+++ resources/views/decks.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Magic - Decks</title>
-</head>
-<body>
+@extends('base')
+
+@section('content')
     @foreach ($decks as $deck)
         <h2>{{ $deck->name }}</h2>
     @endforeach
-</body>
-</html>
+@endsection
