d4828875619786ba57f7e6083dc0cd9e94a7f1f0
Guillaume Perbet <perbet.dev@gmail.com>
2020-10-07 12:23:54 +0200

    add showAll, showDeck, create methods

--- app/Http/Controllers/DeckController.php
+++ app/Http/Controllers/DeckController.php
@@ -4,7 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Deck;
+
 class DeckController extends Controller
 {
-    //
+
+    public function showAll(){
+        
+        //select all decks
+        $decks = Deck::all();
+
+        //return decks view
+        return view( 'decks' , [ 'decks' => $decks ] );
+    }
+
+    public function showDeck( $deck_id ){
+
+        //select deck matching $deck_id
+        $deck = Deck::where( 'id' , $deck_id )->first();
+
+        //return deck view
+        return view( 'deck' , [ 'deck' => $deck ] );
+    }
+
+    public function create(){
+
+        //create new deck with cards field empty { 'cards': [] }
+        $deck = new Deck;
+        $deck->cards = json_encode([ 'cards' => [] ]);
+        $deck->save();
+    }
 }
