330cdf3c2dc1aeade6f6d80faf66f1daeb127766
Guillaume Perbet <perbet.dev@gmail.com>
2020-10-08 11:31:44 +0200

    add navigation links to single decks

--- resources/views/decks.blade.php
+++ resources/views/decks.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     @foreach ($decks as $deck)
-        <h2>{{ $deck->name }}</h2>
+        <h2><a href="/deck/{{ $deck->id }}">{{ $deck->name }}</a></h2>
     @endforeach
 @endsection
