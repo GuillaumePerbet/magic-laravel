6330ab6cc90e50fe773f47bc8c84e409c2228dfa
Guillaume Perbet <perbet.dev@gmail.com>
2020-10-08 12:00:17 +0200

    add edit, update, delete methods

--- app/Http/Controllers/DeckController.php
+++ app/Http/Controllers/DeckController.php
@@ -21,26 +21,51 @@ class DeckController extends Controller
     public function show( $id ){
 
         //select deck matching $id
-        $deck = Deck::find($id);
+        $deck = Deck::find( $id );
 
         //return deck view
-        return view( 'deck' , [ 'deck' => $deck ] );
+        return view( 'deck' , [ 'deck'=>$deck ] );
     }
 
-    public function create( ){
-        //return create form view
-        return view( 'form' );
+    public function create(){
+
+        //return createDeck form view
+        return view( 'createDeck' );
     }
 
     public function store(){
                 
         //insert new Deck with name parameter
         $deck = new Deck;
-        $deck->name = request('name');
+        $deck->name = request( 'name' );
         $deck->save();
 
-        //redirect to Decks index
-        return redirect('/deck');
+        //redirect to decks view
+        return redirect( '/deck' );
     }
 
+    public function edit( $id ){
+
+        //select Deck matching $id
+        $deck = Deck::find( $id );
+
+        //return editDeck form view
+        return view( 'editDeck' , [ 'deck'=>$deck ]);
+    }
+
+    public function update( $id ){
+
+        //update Deck matching $id
+
+        //redirect to updated Deck view
+        return redirect( '/deck/$id' );
+    }
+
+    public function delete( $id ){
+        
+        //delete Deck matching $id
+
+        //redirect to Decks index view
+        return redirect( '/deck' );
+    }
 }
