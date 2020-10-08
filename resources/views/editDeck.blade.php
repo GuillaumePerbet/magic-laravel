5d133585261c319954dc691a38c30fbcce186535
Guillaume Perbet <perbet.dev@gmail.com>
2020-10-08 14:11:11 +0200

    use post protocol in edit form

--- resources/views/editDeck.blade.php
+++ resources/views/editDeck.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
 
 @section('content')
-    <form method="PUT" action="/deck/{{ $deck->id }}">
+    <form method="POST" action="/deck/{{ $deck->id }}">
     @csrf
         <label for="name">Nom du deck</label>
         <input type="text" name="name" id="name" value="{{ $deck->name }}">
